0dc5d1856d468ef268c73333f680fbe5a3839042
romejiang <romejiang@gmail.com>
2016-07-03 02:06:54 +0800

    tuning push script support print error info and port conflict checking

--- scripts/push/deploy.sh
+++ scripts/push/deploy.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
+
+# set -x
 set -e
+shopt -s expand_aliases
 
 ## check nvm 
-if [ -z "$NVM_DIR" ]; then
-  NVM_DIR="$HOME/.nvm"
-fi
+[ -s "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh"
 
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+. /etc/profile
 
-source /etc/profile
-
-alias npm="npm --registry=https://registry.npm.taobao.org \
---cache=$HOME/.npm/.cache/cnpm \
---disturl=https://npm.taobao.org/dist \
---userconfig=$HOME/.cnpmrc"
+alias cnpm="npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc"
 
 APP_DIR=<%=rootPath %><%=appName %>
 DATE=`date '+%Y%m%d%H%M%S'`
@@ -21,23 +17,35 @@ DATE=`date '+%Y%m%d%H%M%S'`
 # save the last known version
 cd $APP_DIR
 if [[ -d current ]]; then
-  sudo cp -Rf current last$DATE
+  cp -Rf current last$DATE
+else
+  mkdir current
 fi
 
-# setup the new version
-if [[ ! -d current ]]; then
-  sudo mkdir current
-fi
-
-sudo mv tmp/bundle.tar.gz current/
+mv tmp/bundle.tar.gz current/
 
 cd current/
 tar xzf bundle.tar.gz
 rm -Rf bundle.tar.gz
 cd bundle/programs/server/
-npm install
+sed -i 's/"resolved.*//g' npm-shrinkwrap.json
+sed -i 's/\("from.*\),$/\1/g' npm-shrinkwrap.json
+cnpm install
 
 cd $APP_DIR
 node --version
 echo "=> Deploy meteor on <%=appName %>"
-pm2 start app.json
+
+pm2 stop app.json
+sleep 1
+
+listen=`netstat -na|grep ':<%=rootPort%> '|grep LISTEN| awk '{print $6}'`
+if [ "$listen"x == "LISTEN"x ]; then
+    echo ""
+    echo "Port <%=rootPort%> has been occupied."
+    echo ""
+    exit 1
+else
+    pm2 start app.json
+fi
+
--- scripts/push/verify.sh
+++ scripts/push/verify.sh
@@ -1,29 +1,25 @@
 APPNAME=<%= appName %>
 DEPLOY_CHECK_WAIT_TIME=<%= deployCheckWaitTime %>
-APP_DIR=/usr/local/meteorup/<%=appName %>
-
-cd $APP_DIR
 
 revert_app (){
-  ~/.nvm/v0.10.45/bin/pm2 logs $APPNAME 1>&2
-  echo ""
-  echo "To see more logs type 'mup logs --tail=50'"
-  echo ""
+  LOGFILE=`pm2 show $APPNAME|grep 'out log path'|awk '{print $6}'`
+  if [ "$LOGFILE"x != ""x ];then
+    tail -n 50  "$LOGFILE"
+    echo ""
+    echo "To see more logs type 'meteorup logs'"
+    echo ""
+  fi
+  echo "Unable to access <%=rootURL %>"
 }
 
 elaspsed=0
 while [[ true ]]; do
   elaspsed=$((elaspsed+1))
-
-  if [ curl $APPNAME.meteorup.cn ]; then
-    revert_app
-    exit 0
-  fi
-
+  curl <%=rootURL %> && exit 0
   sleep 1
 
   if [ "$elaspsed" == "$DEPLOY_CHECK_WAIT_TIME" ]; then
     revert_app
-    exit 0
+    exit 1
   fi
 done
