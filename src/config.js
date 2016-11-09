541ecbddeb9398c5c1b56ebdda34ff45d0ed9880
romejiang <romejiang@gmail.com>
2016-11-10 01:05:07 +0800

    add METEORUP_SERVER_HOST env for debugging

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 exports.url = function() {
     var url = 'http://www.meteorup.cn/';
-    // build_start
-    if (process.env.NODE_ENV === 'development') url = 'http://localhost:3000/';
-    if (process.env.NODE_TEST === 'test') url = 'http://test.meteorup.cn/';
-    // build_end
+    if (process.env.METEORUP_SERVER_HOST) url = process.env.METEORUP_SERVER_HOST;
     return url;
 }
